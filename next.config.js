3a5bbc1873057ea09250473bededdb10dc64a54e
simonchangdevote <simon.chang@devotedigital.com.au>
2026-03-13 10:05:16 +1000

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.cdninstagram.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.fbcdn.net',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
